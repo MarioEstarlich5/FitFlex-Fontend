ad406e5e172d17fd17c7a71838a7552fd3a3289d
Erik Fernandez Torrents <erk.rueda12@gmail.com>
2023-05-09 21:43:03 +0200

    commit erik para fusionar con dietas

--- FitFlex/src/App.jsx
+++ FitFlex/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
         <>
           <AuthHeader/>
           <Routes>
-              <Route path='/inicio' element={<InicioAuth />} />
+              <Route path='/inicio' element={<InicioAuth/>} />
           </Routes>
         </>
        ) : ( 
